2339fa7d1cf2febf45475c5300b9401dc2d404df
Natalie Barbosa <95505439+ShrimplyRight@users.noreply.github.com>
2024-05-31 15:56:33 +0200

    gui.py

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ import threading
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Data Processor")
+        self.title("Data Conversion")
         self.geometry("400x200")
 
         self.label = tk.Label(self, text="Choose a file to process")
